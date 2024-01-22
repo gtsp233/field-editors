f05301b9fdc883afd3742794c57f47e59cd3b4c1
Grant <149294029+gtsp233@users.noreply.github.com>
2024-01-22 00:39:42 -0500

    validate asset URL before downloading

--- packages/reference/src/assets/WrappedAssetCard/AssetCardActions.tsx
+++ packages/reference/src/assets/WrappedAssetCard/AssetCardActions.tsx
@@ -44,6 +44,10 @@ const styles = {
 };
 
 function downloadAsset(url: string) {
+  const isJavaScriptProtocol = /^[\u0000-\u001F ]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*\:/i
+  if (isJavaScriptProtocol.test(url)) {
+    return;
+  }
   window.open(url, '_blank', 'noopener,noreferrer');
 }
 
